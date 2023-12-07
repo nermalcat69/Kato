717d6173778ae4732a1258fd2c1e3635d2d0e4f8
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2023-12-07 09:08:13 +0000

    feat: implemented templated from my other repo

---
+++ components/globals/info.tsx
@@ -0,0 +1,32 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+export default function Info() {
+  return (
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>How do i utilize this ?</AccordionTrigger>
+        <AccordionContent>hello pp</AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="item-2">
+        <AccordionTrigger>How do i utilize this ?</AccordionTrigger>
+        <AccordionContent>Hello pp</AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="item-3">
+        <AccordionTrigger>How do i utilize this ?</AccordionTrigger>
+        <AccordionContent>Hello pp.</AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="item-4">
+        <AccordionTrigger>How do i utilize this ?</AccordionTrigger>
+        <AccordionContent>Hello pp.</AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  )
+}
