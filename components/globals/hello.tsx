a4a35adc1b6c47de13d655751e431e8df54d871b
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2023-12-08 14:51:45 +0000

    fix: hello component

--- components/globals/hello.tsx
+++ components/globals/hello.tsx
@@ -2,7 +2,7 @@ export function Hello() {
   if (process.env.NODE_ENV === "production") return null
 
   return (
-    <div className="fixed hidden md:visible md:block bottom-20 right-10 z-50  px-5 py-3  items-center justify-center rounded-full bg-gray-800 p-3 font-mono text-xs text-white duration-100 hover:bg-gray-900 cursor-pointer">
+    <div className="fixed md:block bottom-20 right-10 z-50  px-5 py-3  items-center justify-center rounded-full bg-gray-800 p-3 font-mono text-xs text-white duration-100 hover:bg-gray-900 cursor-pointer">
       <div className="hidden lg:block font-bold">
         Hello, This website is still work in progress ^_^{" "}
       </div>
